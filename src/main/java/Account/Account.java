bc8c84d452d754922cbedb3019aad0a506d0862f
Delta87 <christian.delta@gmx.net>
2019-12-03 01:31:43 +0100

    Add some Account features. Update Tools

--- src/main/java/Account/Account.java
+++ src/main/java/Account/Account.java
@@ -1,31 +1,107 @@
 package Account;
 
 import Utils.Tools;
-import java.io.File;
+
+import org.json.JSONObject;
+import java.io.*;
 
 public class Account {
 
-    public Account(){
+    private final String API_BASE_URL = "https://authserver.mojang.com";
 
-    checkAccountFile("ZockerDelta");
+
+    public Account() {
+
+        try {
+            checkAccountStatus("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private void checkAccountFile(String accountName)
-    {
-        String fileName = String.format("%s.json",accountName);
+    /**
+     * Check an Minecraft profile status.
+     * @param accountName the useraccount / profile name
+     * @throws IOException
+     */
+    private void checkAccountStatus(String accountName) throws IOException {
+        String fileName = String.format("%s.json", accountName);
         String directory = "accounts";
+        String fullFileName = String.format("%s/%s", directory, fileName);
 
-        if(!Tools.checkFileExist(fileName, directory)) {
-            String fullFileName = String.format("%s/%s", directory, fileName);
-
-
-
-
+        if (!Tools.checkFileExist(fileName, directory)) {
             File tempFile = new File(Tools.getDirectorys(fullFileName));
             if (!tempFile.exists()) {
                 tempFile.mkdirs();
             }
+
+            String dataAfterLogin = login("", "");
+            Tools.writeDataToFile(dataAfterLogin, fullFileName, "file");
+        } else {
+            String result = validateSession(accountName);
+            //TODO: If result is not an empty String a new accesstoken must be generatet (new Login required) - Make a solution.
         }
+
+    }
+
+    /**
+     * Login with an Mohjang account (user and password) and generate a local json file with accesstoken and more.
+     * @param user the username (mostly an email)
+     * @param password the password
+     */
+    public String login(String user, String password) throws IOException {
+
+        JSONObject apiAgent = new JSONObject();
+        apiAgent.put("name", "Minecraft");
+        apiAgent.put("version", 2);
+
+        JSONObject Json = new JSONObject();
+        Json.put("agent", apiAgent);
+        Json.put("username", user);
+        Json.put("password", password);
+        Json.put("requestUser", true);
+
+        return Tools.sendJsonPostToUrl(API_BASE_URL + "/authenticate", Json);
+    }
+
+    /**
+     * Refresh a Minecraft Session, but the Accesstoken will be get invalied.
+     * @param accountName the Accountname what we want to refresh.
+     */
+    public String refreshSession(String accountName) throws IOException {
+
+        String fileName = String.format("%s.json", accountName);
+        String directory = "accounts";
+        String fullFileName = String.format("%s/%s", directory, fileName);
+
+        JSONObject oldData = new JSONObject(Tools.readFileIntoString(fullFileName));
+
+        JSONObject postData = new JSONObject();
+        postData.put("accessToken", oldData.getString("accessToken"));
+        postData.put("clientToken", oldData.getString("clientToken"));
+        postData.put("requestUser", true);
+
+        return Tools.sendJsonPostToUrl(API_BASE_URL + "/refresh", postData);
+
+    }
+
+    /**
+     * Validate a Minecraft session. Try to keep the user logned in (needed for minecraft server)
+     * @param accountName the useraccount / profile
+     */
+    public String validateSession(String accountName) throws IOException {
+
+        String fileName = String.format("%s.json", accountName);
+        String directory = "accounts";
+        String fullFileName = String.format("%s/%s", directory, fileName);
+
+        JSONObject oldData = new JSONObject(Tools.readFileIntoString(fullFileName));
+
+        JSONObject postData = new JSONObject();
+        postData.put("accessToken", oldData.getString("accessToken"));
+        postData.put("clientToken", oldData.getString("clientToken"));
+
+        return Tools.sendJsonPostToUrl(API_BASE_URL + "/validate", postData);
     }
 }
--- src/main/java/Utils/Tools.java
+++ src/main/java/Utils/Tools.java
@@ -1,26 +1,34 @@
 package Utils;
 
+import org.json.JSONObject;
+
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Tools {
 
     /**
      * Get The Fileextension (suffix after dot)
+     *
      * @param fileName The filename
      */
     public static String getFileExtension(String fileName) {
 
         String dirParts[] = fileName.split("/");
-        if(dirParts.length > 0){
+        if (dirParts.length > 0) {
             return dirParts[dirParts.length - 1].substring(dirParts[dirParts.length - 1].length() - 4);
-        }else{
-            return fileName.substring(fileName.length()-4);
+        } else {
+            return fileName.substring(fileName.length() - 4);
         }
     }
 
     /**
      * Get the Filename without extension (suffix before dot)
+     *
      * @param fileName the file for this operation.
      * @return
      */
@@ -28,23 +36,24 @@ public class Tools {
 
         String dirParts[] = fileName.split("/");
 
-        if(dirParts.length > 0){
+        if (dirParts.length > 0) {
             return dirParts[dirParts.length - 1].substring(0, dirParts[dirParts.length - 1].length() - 4);
-        }else {
-            return fileName.substring(0, fileName.length() -4);
+        } else {
+            return fileName.substring(0, fileName.length() - 4);
         }
     }
 
 
     /**
      * Get the directorystructure from a path with filename
+     *
      * @param fileName the filenname where want to get the directorys
      */
     public static String getDirectorys(String fileName) {
 
         String fileDir = "";
         String dirParts[] = fileName.split("/");
-        if(dirParts.length > 0) {
+        if (dirParts.length > 0) {
             for (int i = 0; i < dirParts.length - 1; i++) {
                 fileDir = String.format("%s%s/", fileDir, dirParts[i]);
             }
@@ -54,8 +63,9 @@ public class Tools {
 
     /**
      * Check if the file exists in the dirctory
+     *
      * @param filename the filename to check
-     * @param rootDir the directory which will append first for check (if it is in an subdirectory)
+     * @param rootDir  the directory which will append first for check (if it is in an subdirectory)
      */
     public static boolean checkFileExist(String filename, String rootDir) {
 
@@ -68,9 +78,10 @@ public class Tools {
 
     /**
      * Write data to a file
-     * @param data the data as String or a weburl to a file (for download)
+     *
+     * @param data     the data as String or a weburl to a file (for download)
      * @param fileName the filename after write or download it
-     * @param type file or url to define the operation.
+     * @param type     file or url to define the operation.
      */
     public static void writeDataToFile(String data, String fileName, String type) throws IOException {
 
@@ -81,7 +92,7 @@ public class Tools {
                 os.write(data.getBytes(), 0, data.length());
             } catch (IOException e) {
                 e.printStackTrace();
-            }finally{
+            } finally {
                 try {
                     os.close();
                 } catch (IOException e) {
@@ -99,6 +110,58 @@ public class Tools {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    /**
+     * Get the text input from a file and return it as stromg
+     * @param fileName the file what we want to read
+     */
+    public static String readFileIntoString(String fileName) {
+
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return content;
+    }
+
+    /**
+     * Send a Json String to an Web-API with post parameter
+     * @param purl the url to send the data
+     * @param Json the json data to send
+     */
+    public static String sendJsonPostToUrl(String purl, JSONObject Json) throws IOException {
+        URL url = null;
+        try {
+            url = new URL(purl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = Json.toString().getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+            return response.toString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "";
         }
     }
 }
